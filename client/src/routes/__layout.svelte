14188df75be4c8fbcb19f260c45766ab292d118d
Tanat <ttangun1@gmail.com>
2022-03-21 15:10:09 +0700

    feat(ui): add navbar, about, and some layouts

--- client/src/routes/__layout.svelte
+++ client/src/routes/__layout.svelte
@@ -1,5 +1,9 @@
 <script>
     import "../app.css";
+    import Navbar from "$lib/navbar.svelte";
 </script>
 
-<slot />
+<div class="max-w-3xl m-auto p-2 mt-6">
+    <Navbar />
+    <slot></slot>
+</div>
---
+++ client/src/routes/about.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+  <title>About</title>
+</svelte:head>
+
+<div class="mt-5">
+  <h1>WHAT IS HAPPENING</h1>
+</div>
