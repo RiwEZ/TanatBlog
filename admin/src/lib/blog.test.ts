10afe635c79a7b6562f229d86ac1d6541de28ae6
Tanat <ttangun1@gmail.com>
2022-04-20 20:49:31 +0700

    test: add test for BlogManager edit, delete, slug
    feat: implment BlockManager edit, delte, slug functions

--- admin/src/lib/blog.test.ts
+++ admin/src/lib/blog.test.ts
@@ -1,38 +1,92 @@
-import type { Blog } from './blog';
+import type { Blog, BlogContent } from './blog';
 import BlogManager from './blog';
-import { readFileSync, existsSync, unlinkSync } from 'fs';
+import { readFileSync, existsSync, unlinkSync, writeFileSync, stat } from 'fs';
 import yaml from 'js-yaml';
 
-test('Can access upper folder', () => {
+const path = './tests/_data/blogs';
+const bm = new BlogManager(path);
+
+test('Can access upper folder?', () => {
 	const bm = new BlogManager('../src/_data/blogs');
 	const blogs = bm.getAll();
 	expect(blogs).toBeDefined();
 });
 
-test('Can add new blog', () => {
-	const path = './tests/_data/blogs';
-	const bm = new BlogManager(path);
+test('BlockManager slug behaviour', () => {
+	const bm = new BlogManager('');
+	expect(bm.slug('test add')).toEqual('test-add');
+	expect(bm.slug('Testadd ')).toEqual('testadd');
+	expect(bm.slug(' Test add ')).toEqual('test-add');
+});
 
-	const title = 'test';
-	const date = new Date().toISOString();
-	const blog: Blog = {
+test('Can add new blog?', () => {
+	const title = 'test add';
+	const blog: BlogContent = {
 		title,
 		description: 'test blog',
-		content: 'Lorem .....',
-		slug: title,
-		createdAt: date,
-		updatedAt: date
+		content: 'Lorem .....'
 	};
 
 	const ret = bm.add(blog);
 	expect(ret).toEqual(true);
 
-	const resultPath = `${path}/${title}.yaml`;
-	const result = yaml.load(readFileSync(resultPath, 'utf-8')) as Blog;
+	// file name is a slug of title
+	const resultPath = `${path}/test-add.yaml`;
+	const result = yaml.load(readFileSync(resultPath, 'utf-8')) as BlogContent;
 
 	expect(result).toBeDefined();
-	expect(result).toEqual(blog);
+	expect(result.title).toEqual(blog.title);
+	expect(result.description).toEqual(blog.description);
+	expect(result.content).toEqual(blog.content);
 
 	// remove that blog after done testing
 	if (existsSync(resultPath)) unlinkSync(resultPath);
+});
+
+test('Can delete blog?', () => {
+	const blogSlug = 'test-delete';
+
+	// create mock blog
+	writeFileSync(`${path}/${blogSlug}.yaml`, '...');
+
+	const ret = bm.delete(blogSlug);
+	expect(ret).toEqual(true);
+
+	expect(existsSync(`${blogSlug}.yaml`)).toEqual(false);
+});
+
+test('Can edit blog?', () => {
+	const slug = 't2';
+	const oldUpdate = '2022-04-10T11:45:46.229Z';
+
+	const blog: BlogContent = {
+		title: slug,
+		description: 'ah',
+		content: 'LOREM'
+	};
+
+	// edit 'test-edit' blog
+	const ret = bm.edit('test-edit', blog);
+	expect(ret).toEqual(true);
+
+	const editedBlog = yaml.load(readFileSync(`${path}/${slug}.yaml`, 'utf-8')) as Blog;
+
+	expect(editedBlog.title).toEqual(blog.title);
+	expect(editedBlog.description).toEqual(blog.description);
+	expect(editedBlog.content).toEqual(blog.content)
+
+	expect(editedBlog.updatedAt > oldUpdate).toEqual(true);
+
+	// reset test-edit
+	const backup: Blog = {
+		title: 'Test edit',
+		description: 'YAAA',
+		content: '..',
+		htmlContent: '..',
+		slug: 'test-edit',
+		createdAt: oldUpdate,
+		updatedAt: oldUpdate
+	}
+	unlinkSync(`${path}/${slug}.yaml`);
+	writeFileSync(`${path}/test-edit.yaml`, yaml.dump(backup));
 });
--- admin/src/lib/blog.ts
+++ admin/src/lib/blog.ts
@@ -1,11 +1,15 @@
 import type { JSONObject } from '@sveltejs/kit/types/private';
 import yaml from 'js-yaml';
-import { readdirSync, readFileSync, existsSync, writeFileSync } from 'fs';
+import { readdirSync, readFileSync, existsSync, writeFileSync, unlinkSync, renameSync } from 'fs';
 
-export interface Blog extends JSONObject {
+export interface BlogContent extends JSONObject {
 	title: string;
 	description: string;
 	content: string;
+}
+
+export interface Blog extends BlogContent {
+	htmlContent: string;
 	slug: string;
 	createdAt: string;
 	updatedAt: string;
@@ -15,25 +19,77 @@ export default class BlogManager {
 	path: string;
 
 	/**
-	 * Construct an Article object.
-	 * @param path articles location.
+	 * Construct an BlogManger object with a path to directory to store all blogs.
+	 * @param path directory location
 	 */
 	constructor(path: string) {
 		this.path = path;
 	}
 
-	add(blog: Blog): boolean {
-		const path = `${this.path}/${blog.slug}.yaml`;
+	slug(title: string): string {
+		return title.toLowerCase().trim().replace(/ /g, '-').replace(/[.]/g, '');
+	}
+
+	/**
+	 * Add a new blog to path,
+	 * @param blog blog data
+	 * @returns true if sucess else false
+	 */
+	add(blog: BlogContent): boolean {
+		const slug = this.slug(blog.title);
+		const path = `${this.path}/${slug}.yaml`;
 
 		if (!existsSync(path)) {
-			writeFileSync(path, yaml.dump(blog));
+			const date = new Date().toISOString();
+			const data = blog as Blog;
+
+			data.htmlContent = blog.content; // TODO
+			data.slug = slug;
+			data.createdAt = date;
+			data.updatedAt = date;
+
+			writeFileSync(path, yaml.dump(data));
+			return true;
+		}
+
+		return false;
+	}
+
+	delete(slug: string): boolean {
+		const path = `${this.path}/${slug}.yaml`;
+
+		if (existsSync(path)) {
+			unlinkSync(path);
 			return true;
 		}
 		return false;
 	}
 
+	edit(slug: string, newContent: BlogContent): boolean {
+		const path = `${this.path}/${slug}.yaml`;
+		if (!existsSync(path)) return false;
+
+		const blog = yaml.load(readFileSync(path, 'utf-8')) as Blog;
+		blog.updatedAt = new Date().toISOString();
+
+		blog.title = newContent.title;
+		blog.description = newContent.description;
+		blog.content = newContent.content;
+		blog.htmlContent = newContent.content; // TODO
+
+		if (blog.slug != this.slug(newContent.title)) {
+			// rename file
+			blog.slug = this.slug(newContent.title);
+			const newPath = `${this.path}/${blog.slug}.yaml`;
+			renameSync(path, newPath);
+			writeFileSync(newPath, yaml.dump(blog));
+		} else writeFileSync(path, yaml.dump(blog));
+
+		return true;
+	}
+
 	/**
-	 *
+	 * Get all blogs in path,
 	 * @returns all blogs in path
 	 */
 	getAll(): Blog[] {
