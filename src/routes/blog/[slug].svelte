66cb33c3e6a2dc33fffb51da7534475270c09c32
Tanat <ttangun1@gmail.com>
2022-05-02 00:36:38 +0700

    feat(ui): changin from disqus to utteranc for commenting
    (cherry picked from commit 9300ed9a43870aa42ee85c2223a7cabe45560160)

--- src/routes/blog/[slug].svelte
+++ src/routes/blog/[slug].svelte
@@ -31,8 +31,8 @@
   <meta name="description" content={post.description} />
 </svelte:head>
 
-<div class="mt-10 md:flex">
-  <div class="sm:max-w-3xl 3xl:max-w-7xl">
+<div class="mt-10">
+  <div class="mb-10 sm:max-w-3xl 3xl:max-w-7xl">
     <h1 class="text-4xl font-bold leading-tight">{post.title}</h1>
     <p class="mt-1 text-zinc-400">
       {reading_time.minutes} minutes read • Last updated
@@ -46,4 +46,14 @@
     </p>
     <Content content={post.content} />
   </div>
+
+  <script
+    src="https://utteranc.es/client.js"
+    repo="RiwEZ/TanatBlog"
+    issue-term="title"
+    label="Comment"
+    theme="icy-dark"
+    crossorigin="anonymous"
+    async>
+  </script>
 </div>
