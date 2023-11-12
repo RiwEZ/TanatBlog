f04c0d3721a87a164506c09af4a2bbe3bd4cba14
Tanat <ttangun1@gmail.com>
2023-11-13 05:50:24 +0700

    feat: add prerender entries for each blog

--- svelte.config.js
+++ svelte.config.js
@@ -12,11 +12,16 @@ const config = {
     prerender: {
       crawl: true,
       // we need to specify all route to blogs here, or else it will not prerendered
-      entries: ['*', 
+      entries: [
+        '*', 
         '/blog/[slug]', 
         '/api/posts/changing-from-strapi-to-my-owm-cms.json', 
         '/api/posts/how-i-use-strapi-as-cms-for-this-blog.json',
-        '/api/posts/my-experiences-as-an-backend-intern-at-amity.json']
+        '/api/posts/my-experiences-as-an-backend-intern-at-amity.json',
+        '/blog/changing-from-strapi-to-my-owm-cms', 
+        '/blog/how-i-use-strapi-as-cms-for-this-blog',
+        '/blog/my-experiences-as-an-backend-intern-at-amity',
+      ]
     },
     paths: {
       base: process.env.NODE_ENV === 'development' ? '' : '/TanatBlog'
