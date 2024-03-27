52d3721168e1c8517826059389cecf848e639454
Tanat <ttangun1@gmail.com>
2024-03-27 22:10:20 +0700

    feat: some initial code for migrating to astro

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: 'content',
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		createdAt: z.coerce.date(),
+		updatedAt: z.coerce.date().optional(),
+	}),
+});
+
+export const collections = { blog };
